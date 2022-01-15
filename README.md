4f14d128fa369841a69bd546cd66637f14335b11
Darby Johnson <darbyjjohnson1@gmail.com>
2022-01-15 16:36:13 -0500

    updated all fields to get app working properly

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # EmployeeTeamInfo
+  # Title 
+    Employee Team Info 
+  # Description 
+   Allows employees to enter their information then generates employee information on an HTML
+  # Table of Contents: <br> 
+   * [Installation Instructions](#installation)
+   * [Usage Information](#usage)
+      * [How to Use Demo](#usage)
+   * [Contribution Guidelines](#contribution)
+   * [Test Instructions](#tests)
+   * [License](#license) 
+  ## Installation 
+   node index.js will bring up the prompts
+  ## Usage 
+  just open the created HTML into default browser <br>
+  [How to use](https://watch.screencastify.com/v/dktGsRnwPfWbJ92Uxqqk)
+  ## Contribution 
+  N/A <br>
+  [Contributor Covenant](https://www.contributor-covenant.org/)
+  ## Tests
+   N/A
+  ## Questions: <br> 
+       *Github: https://github.com/darbyjenks
+       *Email: darbyj1707@yahoo.com
--- Utils/htmlRenderer.js
+++ Utils/htmlRenderer.js
@@ -1,32 +1,48 @@
-function makeCardArray(team){
-   const managerArray = team.filter(employee => employee.getRole() === 'Manager');
-//    return JSON.stringify(managerArray, null);
-    return managerArray.toString();
-//    .replace(/[\W_]+/g," ")
-   console.log((managerArray));
-   const internArray = team.filter(employee => employee.getRole() === 'Intern');
-   console.log(internArray);
-//    return internArray;
-   const engineerArray = team.filter(employee => employee.getRole() === 'Engineer');
-   console.log(engineerArray);
-//    return engineerArray;
-}
-//each type of employee... make sure value returned is a string
-function makeHtml(team){
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Team</title>
-</head>
-<body>
-    <h1>Our Team</h1>
-    ${makeCardArray(team)}
-</body>
-</html>
-    `
-};
-module.exports = makeHtml;
+function makeCardArray(team, role){
+
+    return team.map(employee => {
+        if(employee.getRole() != role){
+            return;
+        }
+        return Object.keys(employee).map((key) => {
+            return key + ": " + employee[key];
+          }).join("</br>");
+        }).join("</br>");
+ }
+ 
+ function makeHtml(team){
+     return `
+     <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <title>My Team</title>
+ </head>
+ <body>
+<h1>Our Team</h1>
+<div class="card" style="width: 18rem;">
+<div class="card-body" style='background-color: pink'>
+<h5 class = "card-title manager">Managers</h5>
+<p class="card-text">${makeCardArray(team, "Manager")}</p>
+</div>
+</div>
+<div class="card" style="width: 18rem;">
+<div class="card-body" style='background-color: #ddb6c3'>
+<h5 class = "card-title engineer">Engineers</h5>
+${makeCardArray(team, "Engineer")}
+</div>
+</div>
+<div class="card" style="width: 18rem;">
+<div class="card-body" style='background-color: #a7808d'>
+<h5 class = "card-title intern">Interns</h5>
+${makeCardArray(team, "Intern")}
+</div>
+
+ </body>
+ </html>
+     `
+ };
+ module.exports = makeHtml;
